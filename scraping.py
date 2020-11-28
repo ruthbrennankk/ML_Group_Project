395f762aa86d2fbca204a4a3130073f91e317fcc
ruthbrennankk <ruthbrennankk@gmail.com>
2020-11-28 15:27:32 +0000

    feature gathering

--- scraping.py
+++ scraping.py
@@ -26,18 +26,21 @@ def download_and_save_page(url, file):
 #    for link in links:
 #     print(link['href'])
 #
+
 with open('car_test.html', 'r') as f:
     contents = f.read()
 
 soup = BeautifulSoup(contents, 'html.parser')
-#print(soup.find('span', class_='fpa-features__item__text').get_text())
-id_list = soup.find_all('li', class_='fpa-features__item')
-list = soup.find_all('span', class_='fpa-features__item__text')
-print(id_list)
-print(list)
+# #print(soup.find('span', class_='fpa-features__item__text').get_text())
+# id_list = soup.find_all('li', class_='fpa-features__item')
+# list = soup.find_all('span', class_='fpa-features__item__text')
+# print(id_list)
+# print(list)
+#
+# for i in range(len(list)):
+#     print((id_list[i]).get('id'))
+#     print((list[i]).get_text())
 
-for i in range(len(list)):
-    print((id_list[i]).get('id'))
-    print((list[i]).get_text())
+print(soup.find('script', type='application/ld+json'))
 
 #download_and_save_page('https://www.carzone.ie/used-cars/audi/a4/fpa/202003078142361?journey=Search', 'car_test.html')
