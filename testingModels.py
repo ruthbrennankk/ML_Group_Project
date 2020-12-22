81b52d4e1ea538f6e2d15ab236dfdd21e10bc21f
Christine-Day <daych@tcd.ie>
2020-12-22 17:58:03 +0000

    Updating testing file. Not working very well though

--- testingModels.py
+++ testingModels.py
@@ -3,9 +3,12 @@ import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn import linear_model
 from sklearn.linear_model import Lasso
+from sklearn.dummy import DummyRegressor
 
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 
 import matplotlib.pyplot as plt
 
@@ -15,19 +18,55 @@ def generate_gaussian_kernel_function(gamma):
     weights = lambda dists: np.exp(-gamma * (dists ** 2))
     return lambda dists: weights(dists) / np.sum(weights(dists))
 
-def lasso_regr(X, y):
+def MAE(name, model, Xtrain, Xtest, ytrain, ytest):
+    ypred = model.predict(Xtrain)
+    mae_train = mean_absolute_error(ytrain, ypred)
+    ypred = model.predict(Xtest)
+    mae_test = mean_absolute_error(ytest, ypred)
+    #print("The MAE for the %s model with the training data was %f and with the test data was %f"%(name, mae_train, mae_test))
+
+def MSE(name, model, Xtrain, Xtest, ytrain, ytest):
+    ypred = model.predict(Xtrain)
+    mse_train = mean_squared_error(ytrain, ypred, squared=False)
+    ypred = model.predict(Xtest)
+    mse_test = mean_squared_error(ytest, ypred, squared=False)
+    print("The MSE for the %s model with the training data was %f and with the test data was %f" % (name, mse_train, mse_test))
+
+
+def lasso_regr(Xtrain, Xtest, ytrain, ytest):
     C = 5000
-    model = Lasso(alpha=1 / (2 * C), max_iter=100000).fit(X, y)
-    ypred = model.predict(X)
+    model = Lasso(alpha=1 / (2 * C), max_iter=100000).fit(Xtrain, ytrain)
+    MAE("lasso", model, Xtrain, Xtest, ytrain, ytest)
+    MSE("lasso", model, Xtrain, Xtest, ytrain, ytest)
 
-def lin_regr(X, y):
+def lin_regr(Xtrain, Xtest, ytrain, ytest):
     C = 0.1
-    model = linear_model.Ridge(alpha=1 / (2 * C)).fit(X, y)
-    ypred = model.predict(X)
+    model = linear_model.Ridge(alpha=1 / (2 * C)).fit(Xtrain, ytrain)
+    MAE("ridge", model, Xtrain, Xtest, ytrain, ytest)
+    MSE("ridge", model, Xtrain, Xtest, ytrain, ytest)
 
-def kNN_regr(X, y):
+def kNN_regr(Xtrain, Xtest, ytrain, ytest):
     k=100
     gamma=3
     kernel = generate_gaussian_kernel_function(gamma)
-    model = KNeighborsRegressor(n_neighbors=k, weights=kernel).fit(X, y)
-    ypred = model.predict(X)
+    model = KNeighborsRegressor(n_neighbors=k, weights=kernel).fit(Xtrain, ytrain)
+    MAE("kNN", model, Xtrain, Xtest, ytrain, ytest)
+    MSE("kNN", model, Xtrain, Xtest, ytrain, ytest)
+
+def dummy_regr(Xtrain, Xtest, ytrain, ytest):
+    strategy = "mean"
+    model = DummyRegressor(strategy=strategy).fit(Xtrain, ytrain)
+    MAE("dummy", model, Xtrain, Xtest, ytrain, ytest)
+    MSE("dummy", model, Xtrain, Xtest, ytrain, ytest)
+
+def main():
+    #Load data and hold back a test sample of size 0.2
+    X, y = read("g_cars_final.csv")
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X,y, test_size=0.2)
+    lasso_regr(Xtrain, Xtest, ytrain, ytest)
+    lin_regr(Xtrain, Xtest, ytrain, ytest)
+    kNN_regr(Xtrain, Xtest, ytrain, ytest)
+    dummy_regr(Xtrain, Xtest, ytrain, ytest)
+
+
+main();
